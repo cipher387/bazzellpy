c9822f940625d6708f168ec1aec84fb3f9ed552e
Daniel White <DMW314@alumni.bham.ac.uk>
2022-08-17 11:46:11 +0100

    finished searchengi function

--- bazzellpy/functions.py
+++ bazzellpy/functions.py
@@ -16,8 +16,8 @@ def searchengi(query: str) -> dict:
             "Google Index": f"https://www.google.com/search?q=intitle%3Aindex.of+{query}",
             "Google Scholar": f"http://scholar.google.com/scholar?&q={query}",
             "Google Patents": f"https://patents.google.com/?q={query}",
-            "Bing": f'http://bing.com/search?q="{query}"',
-            "Bing News": f'http://bing.com/news/search?q="{query}"',
+            "Bing": f"http://bing.com/search?q={query}",
+            "Bing News": f"http://bing.com/news/search?q={query}",
             "Yahoo": f"http://search.yahoo.com/search?p={query}",
             "Yandex": f"http://www.yandex.com/yandsearch?text={query}",
             "Baidu": f"http://baidu.com/s?wd={query}",
@@ -32,20 +32,20 @@ def searchengi(query: str) -> dict:
             "Onionland": f"https://onionlandsearchengine.com/search?q={query}"
         },
         "Dark web": {
-            "Torch": "",
-            "Tor66": "",
-            "Haystack": "",
-            "Ahmia": "",
-            "SearchDemon": "",
-            "Excavator": "",
-            "GDark": "",
-            "Hidden Reviews": "",
-            "OnionLand": "",
-            "Phobos": "",
-            "Submarine": "",
-            "DeepSearch": "",
-            "OnionCenter": "",
-            "FreshOnion": ""
+            "Torch": f"http://torch4st4l57l2u2vr5wqwvwyueucvnrao4xajqr2klmcmicrv7ccaad.onion/search?query={query}&action=search",
+            "Tor66": f"http://www.tor66sewebgixwhcqfnp5inzp5x5uohhdy3kvtnyfxc2e5mxiuh34iid.onion/search?q={query}",
+            "Haystack": f"http://haystak5njsmn2hqkewecpaxetahtwhsbsa64jom2k22z5afxhnpxfid.onion/?q={query}",
+            "Ahmia": f"http://juhanurmihxlp77nkq76byazcldy2hlmovfu2epvl5ankdibsot4csyd.onion/search/?q={query}",
+            "SearchDemon": f"http://srcdemonm74icqjvejew6fprssuolyoc2usjdwflevbdpqoetw4x3ead.onion/search?q={query}",
+            "Excavator": f"http://2fd6cemt4gmccflhm6imvdfvli3nf7zn6rfrwpsy7uhxrgbypvwf5fad.onion/search/{query}",
+            "GDark": f"http://zb2jtkhnbvhkya3d46twv3g7lkobi4s62tjffqmafjibixk6pmq75did.onion/gdark/search.php?query={query}",
+            "Hidden Reviews": f"http://u5lyidiw4lpkonoctpqzxgyk6xop7w7w3oho4dzzsi272rwnjhyx7ayd.onion/?s={query}",
+            "OnionLand": f"http://3bbad7fauom4d6sgppalyqddsqbf5u5p56b5k5uk2zxsy3d6ey2jobad.onion/search?q={query}",
+            "Phobos": f"http://phobosxilamwcg75xt22id7aywkzol6q6rfl2flipcqoc4e4ahima5id.onion/search?query={query}",
+            "Submarine": f"http://no6m4wzdexe3auiupv2zwif7rm6qwxcyhslkcnzisxgeiw6pvjsgafad.onion/search.php?term={query}",
+            "DeepSearch": f"http://searchgf7gdtauh7bhnbyed4ivxqmuoat3nm6zfrg3ymkq6mtnpye3ad.onion/search?q={query}",
+            "OnionCenter": f"http://5qqrlc7hw3tsgokkqifb33p3mrlpnleka2bjg7n46vih2synghb6ycid.onion/index.php?a=search&q={query}",
+            "FreshOnion": f"http://freshonifyfe4rmuh6qwpsexfhdrww7wnt5qmkoertwxmcuvm4woo4ad.onion/?query={query}"
         }
     }
 
