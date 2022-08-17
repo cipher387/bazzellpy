3753f9444b52839d552253790849d2622df842db
Daniel White <DMW314@alumni.bham.ac.uk>
2022-08-17 10:23:55 +0100

    Began defining functions

---
+++ bazzell-py/functions.py
@@ -0,0 +1,239 @@
+def searchengi(query: str) -> dict:
+    """
+    Output a dict containing the URLs for your query across all search engines.
+
+    :param query: what you want to search
+    :return: dict containing URLs for your query
+    """
+
+    SEdict = {
+        "Surface web": {
+            "Google": f"http://google.com/search?q={query}",
+            "Google Date": f"http://google.com/search?q={query}&tbs=cdr:1,cd_min:1/1/0,sbd:1",
+            "Google News": f"http://www.google.com/search?tbm=nws&q={query}",
+            "Google Blogs": f"https://www.google.com/search?q={query}&tbm=nws&tbs=nrt:b",
+            "Google FTP": f"https://www.google.com/search?q=inurl%3Aftp%20-inurl%3A(http|https)%20{query}",
+            "Google Index": f"https://www.google.com/search?q=intitle%3Aindex.of+{query}",
+            "Google Scholar": f"http://scholar.google.com/scholar?&q={query}",
+            "Google Patents": f"https://patents.google.com/?q={query}",
+            "Bing": f'http://bing.com/search?q="{query}"',
+            "Bing News": f'http://bing.com/news/search?q="{query}"',
+            "Yahoo": f"http://search.yahoo.com/search?p={query}",
+            "Yandex": "",
+            "Baidu": "",
+            "Searx": "",
+            "Exalead": "",
+            "DuckDuckGo": "",
+            "StartPage": "",
+            "Qwant": "",
+            "Brave": "",
+            "Wayback": "",
+            "Ahmia": "",
+            "Onionland": ""
+        },
+        "Dark web": {
+            "Torch": "",
+            "Tor66": "",
+            "Haystack": "",
+            "Ahmia": "",
+            "SearchDemon": "",
+            "Excavator": "",
+            "GDark": "",
+            "Hidden Reviews": "",
+            "OnionLand": "",
+            "Phobos": "",
+            "Submarine": "",
+            "DeepSearch": "",
+            "OnionCenter": "",
+            "FreshOnion": ""
+        }
+    }
+
+    return SEdict
+
+def facebook(query: str) -> dict:
+    """
+    """
+    FBdict = {
+
+    }
+    return FBdict
+
+def twitter(query: str) -> dict:
+    """
+    """
+    Twitdict = {
+        
+    }
+    return Twitdict
+
+def instagram(query: str) -> dict:
+    """
+    """
+    Instadict = {
+        
+    }
+    return Instadict
+
+def linkedin(query: str) -> dict:
+    """
+    """
+    Linkdict = {
+        
+    }
+    return Linkdict
+
+def communities(query: str) -> dict:
+    """
+    """
+    Commdict = {
+        
+    }
+    return Commdict
+
+def emailaddr(query: str) -> dict:
+    """
+    """
+    Emaildict = {
+        
+    }
+    return Emaildict
+
+def usernameQ(query: str) -> dict:
+    """
+    """
+    Userdict = {
+        
+    }
+    return Userdict
+
+def nameQ(query: str) -> dict:
+    """
+    """
+    Namedict = {
+        
+    }
+    return Namedict
+
+def addressQ(query: str) -> dict:
+    """
+    """
+    Adddict = {
+        
+    }
+    return Adddict
+
+def telephoneNo(query: str) -> dict:
+    """
+    """
+    Nodict = {
+        
+    }
+    return Nodict
+
+def mapQu(query: str) -> dict:
+    """
+    """
+    Mapdict = {
+        
+    }
+    return Mapdict
+
+def doQment(query: str) -> dict:
+    """
+    """
+    Docdict = {
+        
+    }
+    return Docdict
+
+def pastesQ(query: str) -> dict:
+    """
+    """
+    Pastedict = {
+        
+    }
+    return Pastedict
+
+def imgQ(query: str) -> dict:
+    """
+    """
+    Imgdict = {
+        
+    }
+    return Imgdict
+
+def vidQ(query: str) -> dict:
+    """
+    """
+    Vidict = {
+        
+    }
+    return Vidict
+
+def domQ(query: str) -> dict:
+    """
+    """
+    Domdict = {
+        
+    }
+    return Domdict
+
+def IPaddQ(query: str) -> dict:
+    """
+    """
+    IPdict = {
+        
+    }
+    return IPdict
+
+def bizgovQ(query: str) -> dict:
+    """
+    """
+    Instdict = {
+        
+    }
+    return Instdict
+
+def vehiQl(query: str) -> dict:
+    """
+    """
+    Vehdict = {
+        
+    }
+    return Vehdict
+
+def Qrypto(query: str) -> dict:
+    """
+    """
+    currdict = {
+        
+    }
+    return currdict
+
+def breachQ(query: str) -> dict:
+    """
+    """
+    Breachdict = {
+        
+    }
+    return Breachdict
+
+def liveAud(query: str) -> dict:
+    """
+    """
+    Audict = {
+        
+    }
+    return Audict
+
+def liveVid(query: str) -> dict:
+    """
+    """
+    Vidict = {
+        
+    }
+    return Vidict
+
+# TODO: possible future implementation of API search tool
+# see: https://inteltechniques.com/tools/API.html
