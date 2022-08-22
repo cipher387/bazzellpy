96a4ec54b486d892cd64df6b5c2472445f89f105
Daniel White <DMW314@alumni.bham.ac.uk>
2022-08-22 12:43:48 +0100

    v1.0.0

--- setup.py
+++ setup.py
@@ -5,15 +5,16 @@ setup(
     packages=find_packages(include=['bazzellpy']),
     version='1.0.0',
     description='IntelTechniques OSINT tools as callable functions in Python!',
-    readme='README.md'
+    long_description='bazzellpy allows you to call IntelTechniques Search Tools as functions in your Python program. This useful suite of OSINT tools are a great start for any investigation. I wanted to be able to call on these tools without having to re-paste code when working on more complex projects. I have made this library available so you can too.',
+    readme='README.md',
     author='dmw94',
-    license={ file = 'LICENSE' },
+    license='GPLv3',
     classifiers= [
-        'License :: OSI Approved :: GPLv3',
+        'License :: OSI Approved',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-    ]
-    keywords= ['osint', 'searching', 'scraping']
+    ],
+    keywords=['osint', 'searching', 'scraping'],
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
